d9e498369fbad64b918fe23f455020363adbb124
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-01-27 13:19:55 +0300

    Added a conditional to the javascript and Centered everythinh

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 function createGrid(size) {
     let space = document.querySelector(".grid-space");
-
-    for (let i = 0; i < size; i++) {
-        let column = document.createElement("div");
-        column.id = "column";
-        
-        for (let j = 0; j < size; j++){
-            let row = document.createElement("div");
-            row.id = "row";
-            row.style.border = "2px solid black";
-            column.appendChild(row);
+    if (size <= 100) {
+        for (let i = 0; i < size; i++) {
+            let column = document.createElement("div");
+            column.id = "column";
+            
+            for (let j = 0; j < size; j++){
+                let row = document.createElement("div");
+                row.id = "row";
+                row.style.border = "2px solid black";
+                column.appendChild(row);
+            }
+            space.appendChild(column);
         }
-        space.appendChild(column);
+    }
+    else {
+        alert ("No More than 100!")
     }
 }
 
 createGrid(16);
+
+
