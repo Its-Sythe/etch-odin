95b61ce652717f2f5dc6151c2f094e6762db9d3d
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-01-27 12:58:46 +0300

    Made the grid function functional

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-let numberOfDivs = 256;
+function createGrid(size) {
+    let space = document.querySelector(".grid-space");
 
-function createGrid() {
-    for (let i = 1; i <= numberOfDivs; i++) {
-        let divs = document.createElement("div");
-        divs.id = "grid-div";
-        document.querySelector('.grid-space').appendChild(divs)
+    for (let i = 0; i < size; i++) {
+        let column = document.createElement("div");
+        column.id = "column";
+        
+        for (let j = 0; j < size; j++){
+            let row = document.createElement("div");
+            row.id = "row";
+            row.style.border = "2px solid black";
+            column.appendChild(row);
+        }
+        space.appendChild(column);
     }
 }
 
-createGrid();
+createGrid(16);
